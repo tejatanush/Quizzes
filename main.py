d1c9bde88552bbe80ba2536adede48bddc50b2ad
Teja Tanush <tejatanush47@gmail.com>
2025-07-15 15:42:12 +0530

    done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import quiz
 import uvicorn
+import os
 app = FastAPI()
 
 
@@ -16,4 +17,5 @@ app.add_middleware(
 app.include_router(quiz.quiz_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # Use 8000 locally, dynamic in Railway
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
